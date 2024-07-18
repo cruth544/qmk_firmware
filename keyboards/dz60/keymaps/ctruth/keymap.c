91e22de323e1d03ed52a988309a62cf4ae582fa0
ctruth544 <c.ruth544@gmail.com>
2024-07-17 17:17:37 -0700

    Add vim e/b, also use function pointers to reduce redunency

--- keyboards/dz60/keymaps/ctruth/keymap.c
+++ keyboards/dz60/keymaps/ctruth/keymap.c
@@ -26,13 +26,11 @@ const char* tp =
 #include "tp.glsl"
 };
 
-enum remap_keycodes {
-	KC_GESC = QK_GESC
-};
-
 enum custom_keycodes {
   WIN_SWP = SAFE_RANGE,
   DEL_W,
+	FOR_W,
+	BCK_W,
   S_GP,
   S_TP
 };
@@ -40,7 +38,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+				KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         LSFT_FN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, HHKB_FN,
         KC_LCTL, KC_LALT, KC_LGUI,          KC_SPC,  MOUS_FN,        KC_SPC,             KC_RGUI, KC_RALT, KC_RCTL, KC_RCTL, HHKB_FN
@@ -56,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [VIM] = LAYOUT(
         KC_GRV , _______, _______, _______, KC_END , _______, KC_HOME, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_TAB , _______, DEL_W  , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_TAB , _______, DEL_W  , FOR_W  , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_TRNS, _______, _______, KC_DEL , KC_BSPC, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,          KC_ENT ,
-        KC_TRNS, KC_TRNS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, _______, _______, _______, _______, BCK_W  , _______, _______, _______, _______, _______, KC_TRNS, KC_TRNS,
         KC_LCTL, KC_LALT, KC_LGUI,          _______, _______,          _______,          KC_RGUI, _______, KC_RCTL, KC_RCTL, _______
     ),
 
@@ -72,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MOUS] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______,
+        _______, _______, KC_BTN2, KC_MS_U, KC_BTN1, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______,
         _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,          _______, _______,          _______,          _______, _______, _______, _______, _______
@@ -108,8 +106,18 @@ static bool lsfn_down = false;
 static bool spc_down  = false;
 static bool win_swap  = false;
 
+// Function Pointers to conditionally use macros
+uint16_t l_alt_macro(uint16_t kc) 
+{
+	return LALT(kc);
+}
+uint16_t l_ctl_macro(uint16_t kc) 
+{
+	return LCTL(kc);
+}
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
+		uint16_t (*mod_func)(uint16_t) = NULL;
 		uint16_t key        = keycode,
 						 lgui_key   = win_swap ? KC_LALT : KC_LGUI,
              lalt_key   = win_swap ? KC_LGUI : KC_LALT,
@@ -286,15 +294,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     case LSFT_FN:
         if (record->event.pressed)
         {
-          register_code(key);
           lsfn_down = true;
-          /* lsft_down = true; */
         }
         else
         {
-          unregister_code(key);
           lsfn_down = false;
-          /* lsft_down = false; */
+					if (!lsft_down) {
+						unregister_code(KC_LSFT);
+					}
         }
 
         break;
@@ -440,13 +447,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         return false;
 				break;
     case DEL_W:
+    case FOR_W:
+    case BCK_W:
+				mod_func = win_swap ? l_ctl_macro : l_alt_macro;
+				uint16_t action = KC_NO;
+				switch (key) 
+				{
+					case DEL_W:
+						action = KC_BSPC;
+						break;
+					case FOR_W:
+						action = KC_RGHT;
+						break;
+					case BCK_W:
+						action = KC_LEFT;
+						break;
+				}
         if (record->event.pressed)
         {
-            register_code16(A(KC_BSPC));
+					register_code16(mod_func(action));
         }
         else
         {
-            unregister_code16(A(KC_BSPC));
+					unregister_code16(mod_func(action));
         }
 
         return false;
