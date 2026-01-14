960f9f7c1d2a07c4cc6fb267dc38d311085d816c
cruth544 <c.ruth544@gmail.com>
2026-01-13 16:58:51 -0800

    syncing

--- keyboards/dailycraft/claw44/keymaps/oled/config.h
+++ keyboards/dailycraft/claw44/keymaps/oled/config.h
@@ -18,4 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+<<<<<<< HEAD
 #define OLED_FONT_H "keyboards/claw44/lib/glcdfont.c"
+=======
+#define MASTER_LEFT
+#define OLED_FONT_H "claw44/lib/glcdfont.c"
+>>>>>>> f2c789bb4a (Fix build failures noticed in #24163 CI log. (#24164))
